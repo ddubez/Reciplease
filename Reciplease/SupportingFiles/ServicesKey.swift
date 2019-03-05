b93f4d69a35630231e0d55da56ec11ae120653b9
ddubez <ddubez40@gmail.com>
2019-03-05 22:50:12 +0100

    Project creation

---
+++ Reciplease/SupportingFiles/ServicesKey.swift
@@ -0,0 +1,17 @@
+//
+//  ServicesKey.swift
+//  Reciplease
+//
+//  Created by David Dubez on 05/03/2019.
+//  Copyright © 2019 David Dubez. All rights reserved.
+//
+
+import Foundation
+
+struct ServicesKey {
+    // Storage of private key
+    
+    static let apiKey = "To complete"
+
+}
+
